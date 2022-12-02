c135b8c9e2c1046427dae2286c405fc2615dead0
asupstudent <asupstudent@yandex.ru>
2022-12-02 15:19:58 +0300

    Делает бургерное меню, hero блок на всех страницах мобильной версии, начинает слитизовать блок преимуществ (мобильной версии)

---
+++ source/js/index.js
@@ -0,0 +1,8 @@
+document.querySelector('.main-nav__toggle-open').addEventListener('click', function()
+{
+  document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
+})
+document.querySelector('.main-nav__toggle-close').addEventListener('click', function()
+{
+  document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
+})
