9a2536c5b8d74d01dfa4a7df2365f9430d0c2e13
asupstudent <asupstudent@yandex.ru>
2022-12-11 15:15:59 +0300

    Делает автоматическую высоту textarea (по введенному тексту)

--- source/js/index.js
+++ source/js/index.js
@@ -2,7 +2,13 @@ document.querySelector('.main-nav__toggle-open').addEventListener('click', funct
 {
   document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
 })
+
 document.querySelector('.main-nav__toggle-close').addEventListener('click', function()
 {
   document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
 })
+
+function setHeight(fieldClass) {
+  document.querySelector(fieldClass).style.height = '';
+  document.querySelector(fieldClass).style.height = document.querySelector(fieldClass).scrollHeight + 8 + 'px';
+}
