faaf85a1f8650fb9350037e81e76ce477706c35f
asupstudent <asupstudent@yandex.ru>
2023-01-03 22:33:09 +0300

    Добавляет фавиконки в проект и переменные в JS

--- source/js/index.js
+++ source/js/index.js
@@ -1,16 +1,23 @@
-document.querySelector('.main-nav').classList.remove('main-nav--nojs');
+let main_nav = document.querySelector('.main-nav');
+let main_nav_toggle_open = document.querySelector('.main-nav__toggle-open');
+let main_nav_wrapper = document.querySelector('.main-nav__wrapper');
+let main_nav_toggle_close = document.querySelector('.main-nav__toggle-close');
 
-document.querySelector('.main-nav__toggle-open').addEventListener('click', function()
+main_nav.classList.remove('main-nav--nojs');
+
+main_nav_toggle_open.addEventListener('click', function()
 {
-  document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
+  main_nav_wrapper.classList.toggle('main-nav__wrapper--closed');
 })
 
-document.querySelector('.main-nav__toggle-close').addEventListener('click', function()
+main_nav_toggle_close.addEventListener('click', function()
 {
-  document.querySelector('.main-nav__wrapper').classList.toggle('main-nav__wrapper--closed');
+  main_nav_wrapper.classList.toggle('main-nav__wrapper--closed');
 })
 
 function setHeight(fieldClass) {
-  document.querySelector(fieldClass).style.height = '';
-  document.querySelector(fieldClass).style.height = document.querySelector(fieldClass).scrollHeight + 8 + 'px';
+  let text_area = document.querySelector(fieldClass);
+
+  text_area.style.height = '';
+  text_area.style.height = text_area.scrollHeight + 8 + 'px';
 }
